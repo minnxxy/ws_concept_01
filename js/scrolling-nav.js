80f1636c2a7fc72af80023abe00714fbd6159711
Obosa Osagie-Amayo <prosperosagieamayo@gmail.com>
2016-05-06 19:00:49 -0700

    Added files via upload
    + Contact section
    + Footer

---
+++ js/scrolling-nav.js
@@ -0,0 +1,21 @@
+//jQuery to collapse the navbar on scroll
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+        $("#headerImg").addClass("headerImgResize"); //Header Image Resizer
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+        $("#headerImg").removeClass("headerImgResize"); //Header Image Reverter
+    }
+});
+
+//jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
